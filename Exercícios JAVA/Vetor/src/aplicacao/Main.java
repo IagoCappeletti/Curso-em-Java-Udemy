a16efd06119b6ebc501b374605ed7fcb2dd9a46a
Iago cappeletti <iago_cappeletti@hotmail.com>
2022-12-20 22:29:42 -0300

    Exercícios sobre Vetores

---
+++ Exercícios JAVA/Vetor/src/aplicacao/Main.java
@@ -0,0 +1,30 @@
+package aplicacao;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        double[] vect = new double[n];
+
+        for (int i = 0; i < n; i++) {
+            vect[i] = sc.nextDouble();
+        }
+
+        double soma = 0.0;
+        for (int i =0; i < n; i++){
+            soma += vect[i];
+        }
+
+        double media = soma / n;
+
+        System.out.printf("A media entre os números digitados foi de: %.2f", media);
+
+        sc.close();
+    }
+}
---
+++ Exercícios JAVA/vetorObjClasse/src/programa/Main.java
@@ -0,0 +1,37 @@
+package programa;
+
+import entidade.Produto;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        Produto[] vect = new Produto[n];
+
+        for (int i = 0; i < vect.length; i++) {
+            sc.nextLine();
+            String nome = sc.nextLine();
+            double preco = sc.nextDouble();
+            vect[i] = new Produto(nome, preco);
+            //Leio os produtos, instanciar os objetos e guardar nas posições dos vetores
+        }
+
+        double soma = 0.0;
+        for (int i = 0; i < vect.length; i++){
+            soma += vect[i].getPreco();
+        }
+
+        double media = soma / vect.length;
+        //envés de usar a variavel n, podemos usar o vect.length.
+        //Pq evita ficar usando variaveis que não estão ligadas ao vetor.
+        //Utilizando o vect.length fica mais coeso porque o vetor sabe o tamanho dele.
+
+        System.out.printf("A média dos valores é: %.2f%n", media);
+    }
+}
