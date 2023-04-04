e80edde20e55ac3101d11fa2a37c02e0b8c707cc
Iago Cappeletti <iago_cappeletti@hotmail.com>
2023-04-04 17:21:48 -0300

    Sobreposição, Super e @Override

--- Exercícios JAVA/herança e polimorfismo/Aula01-heranca/src/application/Program.java
+++ Exercícios JAVA/herança e polimorfismo/Aula01-heranca/src/application/Program.java
@@ -7,7 +7,7 @@ import entities.SavingsAccount;
 public class Program {
     public static void main(String[] args) {
 
-        //BusinessAccount  account = new BusinessAccount();
+       /* //BusinessAccount  account = new BusinessAccount();
 
         Account acc = new Account(1001, "Alex", 0.0);
         BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
@@ -33,7 +33,18 @@ public class Program {
             SavingsAccount acc5 = (SavingsAccount)acc3;
             acc5.updateBalance();
             System.out.println("Update!");
+        } */
 
-        }
+        Account acc1 = new Account(1001, "Alex", 1000.0);
+        acc1.withdraw(200.0);
+        System.out.println(acc1.getBalance());
+
+        Account acc2 = new SavingsAccount(1002, "Anna", 1000.0, 0.01);
+        acc2.withdraw(200.0);
+        System.out.println(acc2.getBalance());
+
+        Account acc3 = new BusinessAccount(1003, "Pedro", 1000.0, 500.0);
+        acc3.withdraw(200.0);
+        System.out.println(acc3.getBalance());
     }
 }
