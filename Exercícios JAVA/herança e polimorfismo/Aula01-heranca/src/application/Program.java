02f3f8bdb280924cedb64fbd6308ce19117b8c88
Iago Cappeletti <iago_cappeletti@hotmail.com>
2023-04-04 23:38:36 -0300

    Explicação sobre polimorfismo

--- Exercícios JAVA/herança e polimorfismo/Aula01-heranca/src/application/Program.java
+++ Exercícios JAVA/herança e polimorfismo/Aula01-heranca/src/application/Program.java
@@ -1,50 +1,23 @@
 package application;
 
 import entities.Account;
-import entities.BusinessAccount;
 import entities.SavingsAccount;
 
 public class Program {
     public static void main(String[] args) {
 
-       /* //BusinessAccount  account = new BusinessAccount();
+        Account x = new Account(1020, "Alex", 1000.0);
+        Account y = new SavingsAccount(1021, "Anna", 1000.0, 0.01);
 
-        Account acc = new Account(1001, "Alex", 0.0);
-        BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+        /*O que é polimosfismo?
+         A mesma operação em váriaveis do mesmo tipo Account, mas eles tiveram comportamentos diferentes conforme
+         os objetos as quais elas apontam*/
 
-        //UPCASTING
-        Account acc1 = bacc;
-        Account acc2 = new BusinessAccount(1003, "bob", 0.0, 200.0);
-        Account acc3 = new SavingsAccount(1004, "anna", 0.0, 0.01);
+        x.withdraw(50.0);
+        y.withdraw(50.0);
 
-        //DOWNCASTING
-        //BusinessAccount acc4 = acc2; O compilador vai reclamar porque ele não pode converter uma varivel
-        // do tipo Accont para BusinessAccount, pois ele não estabelece uma converção segura
-        BusinessAccount acc4 = (BusinessAccount) acc2;
-        acc4.loan(100);
+        System.out.println(x.getBalance());
+        System.out.println(y.getBalance());
 
-        if (acc3 instanceof BusinessAccount){
-            BusinessAccount acc5 = (BusinessAccount)acc3;
-            acc5.loan(200);
-            System.out.println("Loan!");
-        }
-
-        if (acc3 instanceof SavingsAccount){
-            SavingsAccount acc5 = (SavingsAccount)acc3;
-            acc5.updateBalance();
-            System.out.println("Update!");
-        } */
-
-        Account acc1 = new Account(1001, "Alex", 1000.0);
-        acc1.withdraw(200.0);
-        System.out.println(acc1.getBalance());
-
-        Account acc2 = new SavingsAccount(1002, "Anna", 1000.0, 0.01);
-        acc2.withdraw(200.0);
-        System.out.println(acc2.getBalance());
-
-        Account acc3 = new BusinessAccount(1003, "Pedro", 1000.0, 500.0);
-        acc3.withdraw(200.0);
-        System.out.println(acc3.getBalance());
     }
 }
