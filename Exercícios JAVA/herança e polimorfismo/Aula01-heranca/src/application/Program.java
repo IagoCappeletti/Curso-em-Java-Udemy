07b72ac4ec2dc51f864c8f44edaf3cd5226a622f
Iago Cappeletti <iago_cappeletti@hotmail.com>
2023-04-07 11:38:26 -0300

    Classes abstratas

--- Exercícios JAVA/herança e polimorfismo/Aula01-heranca/src/application/Program.java
+++ Exercícios JAVA/herança e polimorfismo/Aula01-heranca/src/application/Program.java
@@ -1,23 +1,36 @@
 package application;
 
 import entities.Account;
+import entities.BusinessAccount;
 import entities.SavingsAccount;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public class Program {
     public static void main(String[] args) {
 
-        Account x = new Account(1020, "Alex", 1000.0);
-        Account y = new SavingsAccount(1021, "Anna", 1000.0, 0.01);
+        Locale.setDefault(Locale.US);
+        List<Account> list = new ArrayList<>();
 
-        /*O que é polimosfismo?
-         A mesma operação em váriaveis do mesmo tipo Account, mas eles tiveram comportamentos diferentes conforme
-         os objetos as quais elas apontam*/
+        list.add(new SavingsAccount(1001, "Alex", 500.0, 00.1));
+        list.add(new BusinessAccount(1002, "Anna", 1000.0, 200.0));
+        list.add(new SavingsAccount(1003, "iago", 300.0, 00.1));
+        list.add(new SavingsAccount(1004, "Ana", 640.0, 00.1));
+        list.add(new BusinessAccount(1005, "Pedro", 900.0, 400.0));
 
-        x.withdraw(50.0);
-        y.withdraw(50.0);
+        double sum = 0.0;
+        for (Account account : list){
+            sum += account.getBalance();
+        }
+        System.out.printf("Total saldos: %.2f%n", sum);
 
-        System.out.println(x.getBalance());
-        System.out.println(y.getBalance());
-
+        for (Account account : list){
+            account.deposit(10.0);
+        }
+        for (Account account : list){
+            System.out.printf("Update balance for account %d: %.2f%n", account.getNumber(), account.getBalance());
+        }
     }
 }
