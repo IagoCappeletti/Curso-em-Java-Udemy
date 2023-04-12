a4caa3711588f2bc1cffcb95f9fecc4cfe66e9e1
Iago Cappeletti <iago_cappeletti@hotmail.com>
2023-04-12 17:44:57 -0300

    Utilização de StackTrace

---
+++ Exercícios JAVA/Exceções/StackTrace/src/Main.java
@@ -0,0 +1,35 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        method1();
+        System.out.println("FIM DO PROGRAMA!");
+    }
+    public static void method1(){
+        System.out.println("***METHOD1 START***");
+        method2();
+        System.out.println("***METHOD1 END ***");
+    }
+    public static void method2(){
+        System.out.println("***METHOD2 START***");
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            String[] vect = sc.nextLine().split(" ");
+            int position = sc.nextInt();
+            System.out.println(vect[position]);
+        }
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("pisição invalida!");
+            e.printStackTrace();
+            sc.next();
+        }
+        catch (InputMismatchException e){
+            System.out.println("Só pode letra");
+        }
+        sc.close();
+        System.out.println("***METHOD2 END***");
+    }
+}
