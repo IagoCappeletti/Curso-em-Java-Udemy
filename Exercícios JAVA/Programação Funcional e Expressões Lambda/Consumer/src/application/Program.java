d74bd33b662c650c6196c06999dbd66080bb4b2a
IagoCappeletti <iago_cappeletti@hotmail.com>
2024-02-17 12:27:55 -0300

    espressão lambda inLine

--- Exercícios JAVA/Programação Funcional e Expressões Lambda/Consumer/src/application/Program.java
+++ Exercícios JAVA/Programação Funcional e Expressões Lambda/Consumer/src/application/Program.java
@@ -15,9 +15,7 @@ public class Program {
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
 
-        Consumer<Product> cons = product -> product.setPrice(product.getPrice() * 1.1);
-
-        list.forEach(cons);
+        list.forEach(product -> product.setPrice(product.getPrice() * 1.1));
         list.forEach(System.out::println);
     }
 }
