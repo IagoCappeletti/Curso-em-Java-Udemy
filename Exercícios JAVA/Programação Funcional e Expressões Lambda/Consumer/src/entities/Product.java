59f6000999aedab837f42bef2a3985b212ec9d53
IagoCappeletti <iago_cappeletti@hotmail.com>
2024-02-17 12:13:55 -0300

    Implementação da interface Consumer

---
+++ Exercícios JAVA/Programação Funcional e Expressões Lambda/Consumer/src/entities/Product.java
@@ -0,0 +1,56 @@
+package entities;
+
+public class Product {
+
+    private  String name;
+    private double price;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Product product = (Product) o;
+
+        if (Double.compare(price, product.price) != 0) return false;
+        if (name != null ? !name.equals(product.name) : product.name != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = name != null ? name.hashCode() : 0;
+        temp = Double.doubleToLongBits(price);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "name: " + name + ", price: " + price;
+    }
+}
