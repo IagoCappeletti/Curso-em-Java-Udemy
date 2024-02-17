2a9194af2867327664799904186356b869e72202
IagoCappeletti <iago_cappeletti@hotmail.com>
2024-02-17 12:22:58 -0300

    referencia method com método não estático

--- Exercícios JAVA/Programação Funcional e Expressões Lambda/Consumer/src/entities/Product.java
+++ Exercícios JAVA/Programação Funcional e Expressões Lambda/Consumer/src/entities/Product.java
@@ -29,6 +29,9 @@ public class Product {
    public static void staticPriceUpdate(Product p) {
         p.setPrice(p.getPrice() * 1.1);
    }
+    public  void NonStaticPriceUpdate() {
+        price = price * 1.1;
+    }
 
     @Override
     public String toString() {
